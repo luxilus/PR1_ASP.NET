8a16798e8f55f121e2db74cfc19f6ce43d63ce35
Luxilus <Luxilus@DESKTOP-B52NH6I>
2022-06-16 11:55:26 +0500

    Добавьте файлы проекта.

---
+++ PR1_ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PR1_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
